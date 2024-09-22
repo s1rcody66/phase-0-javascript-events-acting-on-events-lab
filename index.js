f7efa5b411fb008e15c62666112022433643af51
s1rcody66 <cody200600@gmail.com>
2024-09-23 00:06:19 +0300

    first commit

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Your code here
+const dodger = document.getElementById("dodger");
+function moveDodgerLeft() {
+    const leftNUmbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNUmbers,10);
+
+    if ( left > 0) (
+        dodger.style.left = `${left - 1}pz`
+    )
+}
+function moveDodgerRight() {
+    const leftNUmbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNUmbers,10);
+
+    if ( left > 360) (
+        dodger.style.left = `${left - 1}pz`
+
+    )
+}
+document.addEventListener("keydown", function (e) {
+    if (e.key === "Arrowleft") (
+        moveDodgerLeft();
+    ) else if (e.key === "ArrowRight") (
+        moveDodgerRight();
+    )
+
+}
+    
